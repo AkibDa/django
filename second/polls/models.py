abb163b9e8f798018f6c01ef10880e8039d4f9d7
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-02-04 10:02:56 +0530

    didn't work again

--- second/polls/models.py
+++ second/polls/models.py
@@ -8,3 +8,8 @@ class Question(models.Model):
     # ...
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+class Choice(models.Model):
+    # ...
+    def __str__(self):
+        return self.choice_text
