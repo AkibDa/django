d9338876ee1623f8afd0f8f3cff076c70fde0eac
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-02-04 19:41:32 +0530

    downloaded tailwind

--- third/third/third/settings.py
+++ third/third/third/settings.py
@@ -39,7 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'one',
+    'tailwind',
+    'theme',
 ]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS=['127.0.0.1']
+
+NPM_BIN_PATH = '/usr/local/bin/npm'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
