c5e979220263710e282684a8f3fe6d946d8177e1
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-02-04 11:14:37 +0530

    injected style in the html

--- third/third/third/views.py
+++ third/third/third/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-  return HttpResponse("Hello, This is home page django")
+  #return HttpResponse("Hello, This is home page django")
+  return render(request, 'websites/index.html')
 
 def about(request):
   return HttpResponse("Hello, This is about page django")
