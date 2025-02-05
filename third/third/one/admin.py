cd9b93fb30090a8c48f5c431a7f539c0ca8d7659
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-02-05 11:08:52 +0530

    configured admin panel

--- third/third/one/admin.py
+++ third/third/one/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import OneVarity
+from .models import OneVarity, OneReview, OneCertificate, Store
 
 # Register your models here.
-admin.site.register(OneVarity)
+class OneReviewInLine(admin.TabularInline):
+  model = OneReview
+  extra = 2
+
+class OneVarietyAdmin(admin.ModelAdmin):
+  list_display = ('name','type','date_added')
+  inlines = [OneReviewInLine]
+
+class StoreAdmin(admin.ModelAdmin):
+  list_display = ('name', 'location')
+  filter_horizontal = ('one_varities',)
+
+class OneCertificateAdmin(admin.ModelAdmin):
+  list_display = ('chai','certificate_number')
+
+
+admin.site.register(OneVarity, OneVarietyAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(OneCertificate, OneCertificateAdmin)
+admin.site.register(OneReview, OneReviewInLine)
